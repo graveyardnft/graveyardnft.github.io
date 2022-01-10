b55c2be2ceefc039a64727dff3db14d86af49706
0xyamyam <0xyamyam@gmail.com>
2022-01-10 15:03:04 +0000

    chore(app): Update 404 template

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full h-full flex items-center justify-center">
-    404 Whoops, something is missing here.
+  <div class="container mx-auto text-center pt-8 md:pt-32 px-4">
+    <h1 class="text-xl md:text-3xl">404 Whoops, something is missing here.</h1>
   </div>
 </template>
 
