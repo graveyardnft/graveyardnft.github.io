d900065b6c383ddeeebd6ee7ceaba9b9c1da139e
0xyamyam <0xyamyam@gmail.com>
2022-02-06 12:47:42 +0000

    feature(commit): Provide commit help modal

--- src/components/Commit.vue
+++ src/components/Commit.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="flex flex-col w-96 max-w-full mx-auto p-8 bg-slate-900/90 rounded border-4 border-slate-900 text-grey-100">
+    <a href="#" class="flex items-center justify-center mb-2 text-sm" @click.prevent="toggleHelp">
+      <span>Help</span>
+      <svg viewBox="0 0 24 24" class="w-4 h-4 ml-1 fill-current">
+        <path d="M15.07,11.25L14.17,12.17C13.45,12.89 13,13.5 13,15H11V14.5C11,13.39 11.45,12.39 12.17,11.67L13.41,10.41C13.78,10.05 14,9.55 14,9C14,7.89 13.1,7 12,7A2,2 0 0,0 10,9H8A4,4 0 0,1 12,5A4,4 0 0,1 16,9C16,9.88 15.64,10.67 15.07,11.25M13,19H11V17H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
+      </svg>
+    </a>
     <input v-model="contractAddress" type="text" class="mb-2 p-2 rounded text-2xl bg-transparent text-center placeholder:text-grey-100  outline-none focus:ring ring-slate-600/25" placeholder="Enter contract address" />
     <input v-model="tokenId" type="number" class="mb-2 p-2 rounded text-2xl bg-transparent text-center placeholder:text-grey-100  outline-none focus:ring ring-slate-600/25" placeholder="Enter tokenId" />
     <input v-model="data" type="text" class="mb-6 p-2 rounded text-2xl bg-transparent text-center placeholder:text-grey-100  outline-none focus:ring ring-slate-600/25" placeholder="Last rites" />
     <Button @click="commit(contractAddress, tokenId, data)">Commit To The Graveyard</Button>
   </div>
+  <Modal v-if="helpOpen" @close="toggleHelp">
+    <template #header>
+      Transferring Token To The Graveyard
+    </template>
+    <div class="text-left">
+      <p class="mb-2 text-xl">Contract Address & tokenId</p>
+      <p class="mb-2">Enter the contract address of the token you wish to transfer to the graveyard, and the tokenId.</p>
+      <p class="mb-2">These can be found on OpenSea in the url when you view the item.</p>
+      <pre class="mb-4 max-w-full overflow-scroll">https://opensea.io/assets/{theContractAddress}/{tokenId}</pre>
+      <p class="mb-2 text-xl">Last Rites</p>
+      <p>You can optionally commit your token with a final message of committal, say goodbye to your token.</p>
+    </div>
+  </Modal>
   <Modal v-if="committing" @close="reset">
     <template #header>
       Transferring Token To The Graveyard
@@ -34,6 +53,7 @@ const props = defineProps({
   }
 })
 
+const helpOpen = ref(false)
 const success = inject<(msg: string) => {}>('success')
 const contractAddress = ref('')
 const tokenId = ref<number|null>(null)
@@ -41,6 +61,10 @@ const data = ref('')
 const committing = ref(false)
 const transaction = ref(null)
 const receipt = ref(null)
+
+const toggleHelp = () => {
+  helpOpen.value = !helpOpen.value
+}
 
 const commit = async (contractAddress: string, tokenId: number, data: string) => {
   if (contractAddress === '') throw new Error('Enter the NFT contract address of the token you would like to commit.')
